5a2d04c5e234ee92b4d5ff7958b66b258f247602
Guðmundur Björn Birkisson <gbi@enonic.com>
2021-05-20 15:43:49 +0200

    Remove old ResourceQuery

--- java-operator/src/main/java/com/enonic/kubernetes/operator/helpers/InformerEventHandler.java
+++ java-operator/src/main/java/com/enonic/kubernetes/operator/helpers/InformerEventHandler.java
@@ -1,9 +1,8 @@
 package com.enonic.kubernetes.operator.helpers;
 
+import com.enonic.kubernetes.kubernetes.Predicates;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
-
-import com.enonic.kubernetes.kubernetes.ResourceQuery;
 
 import static com.enonic.kubernetes.common.Configuration.cfgLong;
 
@@ -15,18 +14,17 @@ public abstract class InformerEventHandler<R extends HasMetadata>
     public void initialize()
     {
         long reSync = cfgLong( "operator.informers.reSync" );
-        maxAge = ( reSync / 2L ) / 1000;
+        maxAge = (reSync / 2L) / 1000;
     }
 
     @Override
     public void onAdd( final R newResource )
     {
-        ResourceQuery.resourceQuery( newResource ).
-            youngerThen( maxAge ).
-            get().
-            ifPresent( this::onNewAdd );
+        if (Predicates.youngerThan( maxAge ).test( newResource )) {
+            onNewAdd( newResource );
+        }
     }
 
     protected abstract void onNewAdd( final R newResource );
-    
+
 }
