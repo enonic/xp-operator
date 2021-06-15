82512b02d80aff35212d95d25519013ebf032c70
Guðmundur Björn Birkisson <gbi@enonic.com>
2021-06-15 15:08:43 +0200

    Log action limiter

--- java-operator/src/main/java/com/enonic/kubernetes/kubernetes/ActionLimiter.java
+++ java-operator/src/main/java/com/enonic/kubernetes/kubernetes/ActionLimiter.java
@@ -2,6 +2,8 @@ package com.enonic.kubernetes.kubernetes;
 
 import com.enonic.kubernetes.common.TaskRunner;
 import io.fabric8.kubernetes.api.model.HasMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -11,6 +13,8 @@ import java.util.function.Function;
 
 public class ActionLimiter
 {
+    private static final Logger log = LoggerFactory.getLogger( ActionLimiter.class );
+
     private final TaskRunner taskRunner;
 
     private final Map<Integer, Runnable> ops;
@@ -26,6 +30,7 @@ public class ActionLimiter
         int key = hashFunc.apply( t ).hashCode();
         ops.put( key, r );
         taskRunner.scheduleOneTime( () -> maybeRun( key ), delay, TimeUnit.MILLISECONDS );
+        log.debug( String.format( "Limiter with delay %d scheduled for: %s", delay, r) );
     }
 
     public <T> void limit( long delay, T t, Function<T, Object> hashFunc, Consumer<T> c )
@@ -42,6 +47,7 @@ public class ActionLimiter
     {
         Runnable r = ops.remove( m );
         if (r != null) {
+            log.debug( String.format( "Limiter running for for: %s", r) );
             r.run();
         }
     }
