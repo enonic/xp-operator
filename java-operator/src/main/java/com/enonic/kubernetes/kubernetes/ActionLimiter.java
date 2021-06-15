ad96066e4a0b6cc6bf108c7a2b57e5e837030c3c
Guðmundur Björn Birkisson <gbi@enonic.com>
2021-06-15 14:48:45 +0200

    Use concurrent hashmap in ActionLimiter

--- java-operator/src/main/java/com/enonic/kubernetes/kubernetes/ActionLimiter.java
+++ java-operator/src/main/java/com/enonic/kubernetes/kubernetes/ActionLimiter.java
@@ -3,8 +3,8 @@ package com.enonic.kubernetes.kubernetes;
 import com.enonic.kubernetes.common.TaskRunner;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -18,10 +18,10 @@ public class ActionLimiter
     public ActionLimiter( final TaskRunner taskRunner )
     {
         this.taskRunner = taskRunner;
-        this.ops = new HashMap<>();
+        this.ops = new ConcurrentHashMap<>();
     }
 
-    public synchronized <T> void limit( long delay, T t, Function<T, Object> hashFunc, Runnable r )
+    public <T> void limit( long delay, T t, Function<T, Object> hashFunc, Runnable r )
     {
         int key = hashFunc.apply( t ).hashCode();
         ops.put( key, r );
@@ -38,7 +38,7 @@ public class ActionLimiter
         limit( delay, r, x -> String.format( "%s%s", x.getMetadata().getNamespace(), x.getMetadata().getName() ), c );
     }
 
-    private synchronized void maybeRun( int m )
+    private void maybeRun( int m )
     {
         Runnable r = ops.remove( m );
         if (r != null) {
