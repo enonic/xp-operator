9c8cf6c1d2d1e158635e6f127cfdef00ad694684
Bradnitski <vbr@enonic.com>
2023-10-30 19:21:31 +0100

    add cname records support #495

---
+++ java-operator/src/main/java/com/enonic/kubernetes/operator/domain/LBConfig.java
@@ -0,0 +1,28 @@
+package com.enonic.kubernetes.operator.domain;
+
+
+import java.util.Optional;
+
+import javax.enterprise.inject.Default;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "dns.lb")
+public interface LBConfig
+{
+    @WithName("cname")
+    Optional<String> cname();
+
+    @WithName("staticIp")
+    Optional<String> staticIp();
+
+    @WithName("service.name")
+    String serviceName();
+
+    @WithName("service.namespace")
+    String serviceNamespace();
+
+
+}
