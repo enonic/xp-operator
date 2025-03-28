b69481f302d6b0b0fa980d9fe7f65f8308f3662c
Sergey Rymsha <rymsha@users.noreply.github.com>
2025-03-28 07:57:37 +0100

    More tasks can be run in parallel #670 (#671)
    ---------
    
    Co-authored-by: Bradnitski <vbr@enonic.com>

--- java-operator/src/main/java/com/enonic/kubernetes/common/TaskRunner.java
+++ java-operator/src/main/java/com/enonic/kubernetes/common/TaskRunner.java
@@ -1,12 +1,19 @@
 package com.enonic.kubernetes.common;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Singleton;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Singleton;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import static com.enonic.kubernetes.common.Configuration.cfgInt;
 import static com.enonic.kubernetes.common.SingletonAssert.singletonAssert;
@@ -17,35 +24,48 @@ public class TaskRunner
 {
     private static final Logger log = LoggerFactory.getLogger( TaskRunner.class );
 
-    private final ScheduledThreadPoolExecutor executor;
+    private final ScheduledExecutorService scheduledExecutorService;
+
+    private final ExecutorService executor;
 
     public TaskRunner()
     {
         singletonAssert( this, "constructor" );
-        executor = new ScheduledThreadPoolExecutor( cfgInt( "operator.tasks.threads" ), new ThreadFactoryBuilder().
-            setNameFormat( "taskrunner-%d" ).
-            build() );
+        scheduledExecutorService =
+            Executors.newScheduledThreadPool( 4, new ThreadFactoryBuilder().setNameFormat( "task-scheduler-%d" ).build() );
+
+        executor = new ThreadPoolExecutor( 1, cfgInt( "operator.tasks.threads" ), 60L, TimeUnit.SECONDS, new SynchronousQueue<>(),
+                                           new ThreadFactoryBuilder().setNameFormat( "task-runner-%d" ).build() );
     }
 
     public void scheduleAtFixedRate( final Runnable command, final long initialDelay, final long period, final TimeUnit unit )
     {
-        executor.scheduleAtFixedRate( () -> {
-            try {
-                command.run();
-            } catch (Exception e) {
-                log.error( "Task " + command.getClass().getSimpleName() + " threw exception", e );
-            }
-        }, initialDelay, period, unit );
+        scheduledExecutorService.scheduleAtFixedRate( () -> schedulable( command ), initialDelay, period, unit );
     }
 
-    public void scheduleOneTime( final Runnable command, final long initialDelay, final TimeUnit unit )
+    public ScheduledFuture<?> scheduleOneTime( final Runnable command, final long initialDelay, final TimeUnit unit )
     {
-        executor.schedule( () -> {
-            try {
-                command.run();
-            } catch (Exception e) {
-                log.error( "Task " + command.getClass().getSimpleName() + " threw exception", e );
-            }
-        }, initialDelay, unit );
+        return scheduledExecutorService.schedule( () -> schedulable( command ), initialDelay, unit );
+    }
+
+    private void schedulable( final Runnable command )
+    {
+        try
+        {
+            executor.execute( () -> {
+                try
+                {
+                    command.run();
+                }
+                catch ( Throwable e )
+                {
+                    log.error( "Task {} threw exception", command.getClass().getSimpleName(), e );
+                }
+            } );
+        }
+        catch ( Exception e )
+        {
+            log.error( "Task {} cannot be executed", command.getClass().getSimpleName(), e );
+        }
     }
 }
